3dc7ee7419681102e8c421036c87d1eabfd6b3e0
Anton <sharapov-work@yandex.ru>
2020-06-10 01:45:20 +0700

    ADD DB

--- prices_app/price_app/settings.py
+++ prices_app/price_app/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'price_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'price_app_db',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
